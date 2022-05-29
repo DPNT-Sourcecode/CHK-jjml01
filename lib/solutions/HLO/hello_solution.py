1cde26c1a6dd6b58538c9a8228955eb54ad3e121
sbx_user1051 <sbx_user1051@169.254.9.197>
2022-05-29 14:41:24 +0000

    Sun May 29 14:41:24 UTC 2022

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -3,9 +3,10 @@
 # noinspection PyUnusedLocal
 # friend_name = unicode string
 def hello(friend_name):
-    output = 'Hello, ',friend_name,'!'
+    output = 'Hello, ' + friend_name + '!'
     assert output
     return(output)
+
 
 
 
