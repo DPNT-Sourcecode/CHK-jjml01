80fd71dfd7b98e6332c4ce9cd3f3c63a29fb5ce4
sbx_user1051 <sbx_user1051@169.254.9.197>
2022-05-29 15:44:07 +0000

    Sun May 29 15:44:07 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -6,22 +6,35 @@ def checkout(skus):
 
     itemList = [x for x in skus]
     for item in itemList:
-        if item not in ['A','B','C','D']:
+        if item not in ['A','B','C','D','E']:
             return -1
 
     As = [x for x in itemList if x == 'A']
     Bs = [x for x in itemList if x == 'B']
     Cs = [x for x in itemList if x == 'C']
     Ds = [x for x in itemList if x == 'D']
+    Es = [x for x in itemList if x == 'E']
 
     LenA = len(As)
-    RemainderA = LenA % 3
-    QuotientA = LenA // 3
+    offer200_RemainderA = LenA % 5
+    offer200_QuotientA = LenA // 5
+
+    SingularA = offer200_RemainderA % 3
+    offer150_QuotientA = offer200_RemainderA // 3
+
     LenB = len(Bs)
     RemainderB = LenB % 2
     QuotientB = LenB // 2
+
     LenC = len(Cs)
+
     LenD = len(Ds)
-    print(LenA, LenB, LenC, LenD)
-    finalPrice = (RemainderA * 50) + (QuotientA * 130) + (RemainderB * 30) + (QuotientB * 45) + (LenC * 20) + (LenD * 15)
+
+    LenE = len(Es)
+    RemainderE = LenE % 2
+    QuotientE = LenE // 2
+
+    print(LenA, LenB, LenC, LenD, LenE)
+    finalPrice = (SingularA * 50) + (offer150_QuotientA * 130) + (offer200_QuotientA * 200) + (RemainderB * 30) + (QuotientB * 45) + (LenC * 20) + (LenD * 15) + (QuotientE * 30) + (RemainderE * 40)
     return finalPrice
+
