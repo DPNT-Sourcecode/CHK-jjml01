ad807f09634d5000f19d50cbc45c4eb6db08b645
sbx_user1051 <sbx_user1051@169.254.9.197>
2022-05-29 15:14:35 +0000

    Sun May 29 15:14:35 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,11 +3,17 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
+
     itemList = [x for x in skus]
     As = [x == 'A' for x in itemList]
     Bs = [x == 'B' for x in itemList]
     Cs = [x == 'C' for x in itemList]
     Ds = [x == 'D' for x in itemList]
+
+    LenA = len(As)
+    LenB = len(Bs)
+    LenC = len(Cs)
+    LenD = len(Ds)
 
     priceList = {
         'A': 50,
@@ -16,6 +22,7 @@ def checkout(skus):
         'D': 15
 
     }
+
 
 
 
