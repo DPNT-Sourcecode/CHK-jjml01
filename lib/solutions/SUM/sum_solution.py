76dc51a74988446ac40ba844d3f0e1d0a012840d
sbx_user1051 <sbx_user1051@169.254.9.197>
2022-05-29 14:15:37 +0000

    Sun May 29 14:15:37 UTC 2022

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,13 +1,14 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
 def compute(x, y):
-
+    if 
     if  isinstance(x,int) and isinstance(y,int):
-        
+
         result = sum(x, y)
         assert isinstance(result, int)
         return result
     else:
         pass
+
 
 
 
