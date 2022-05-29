df55d7f2d962a1c28cec53d31542da4d893294f9
sbx_user1051 <sbx_user1051@169.254.9.197>
2022-05-29 14:12:37 +0000

    Sun May 29 14:12:37 UTC 2022

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -4,8 +4,12 @@ def compute(x, y):
     if  isinstance(x,int) and isinstance(y,int):
         
         result = sum(x, y)
+        assert isinstance(result, int)
+        return result
+    else:
+        pass
 
-    assert isinstance(result, int)
-    return result   
+
+
 
 
