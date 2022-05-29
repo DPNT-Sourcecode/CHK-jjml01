3c7f1ad2124f23199a29b2b626c427921a4f87de
sbx_user1051 <sbx_user1051@169.254.9.197>
2022-05-29 14:17:04 +0000

    Sun May 29 14:17:04 UTC 2022

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,11 +1,17 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
 def compute(x, y):
-    if 
-    if  isinstance(x,int) and isinstance(y,int):
+    if x >= 0 and y >= 0:
 
-        result = sum(x, y)
-        assert isinstance(result, int)
-        return result
+        if  isinstance(x,int) and isinstance(y,int):
+
+            result = sum(x, y)
+            if result <= 200:
+                assert isinstance(result, int)
+                return result
+            else:
+                pass
+        else:
+            pass
     else:
         pass
 
