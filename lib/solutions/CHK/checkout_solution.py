4f2900e64012c1641e5c6d9c523433f2236014e9
sbx_user1051 <sbx_user1051@169.254.9.197>
2022-05-29 15:27:20 +0000

    Sun May 29 15:27:20 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -13,7 +13,7 @@ def checkout(skus):
     Bs = [x == 'B' for x in itemList]
     Cs = [x == 'C' for x in itemList]
     Ds = [x == 'D' for x in itemList]
-    assert As
+
     LenA = len(As)
     RemainderA = LenA % 3
     QuotientA = LenA // 3
@@ -22,8 +22,9 @@ def checkout(skus):
     QuotientB = LenB // 2
     LenC = len(Cs)
     LenD = len(Ds)
-
+    print(LenA, LenB, LenC, LenD)
     finalPrice = (RemainderA * 50) + (QuotientA * 130) + (RemainderB * 30) + (QuotientB * 45) + (LenC * 20) + (LenD * 15)
     return finalPrice
+
 
 
