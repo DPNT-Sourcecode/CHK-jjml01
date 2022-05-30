e6ce06fe073c1729ee536d39385d99ec1951d019
sbx_user1051 <sbx_user1051@169.254.70.125>
2022-05-30 13:44:00 +0000

    Mon May 30 13:44:00 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,21 +3,21 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-
+    skuList = ['A','B','C','D','E','F']
     itemList = [x for x in skus]
     for item in itemList:
-        if item not in ['A','B','C','D','E']:
+        if item not in skuList:
             return -1
 
-    As = [x for x in itemList if x == 'A']
-    Bs = [x for x in itemList if x == 'B']
-    Cs = [x for x in itemList if x == 'C']
-    Ds = [x for x in itemList if x == 'D']
-    Es = [x for x in itemList if x == 'E']
+    itemBreakdown = []
 
-    LenA = len(As)
-    offer200_RemainderA = LenA % 5
-    offer200_QuotientA = LenA // 5
+    for item in skuList:
+        itemBreakdown.append([x for x in itemList if x == skuList[skuList.index(item)]])
+
+    itemCount = [len(x) for x in itemBreakdown]
+
+    offer200_RemainderA = itemCount[0] % 5
+    offer200_QuotientA = itemCount[0] // 5
 
     SingularA = offer200_RemainderA % 3
     offer150_QuotientA = offer200_RemainderA // 3
@@ -28,19 +28,20 @@ def checkout(skus):
 
     LenD = len(Ds)
 
-    LenE = len(Es)
-    RemainderE = LenE % 2
-    QuotientE = LenE // 2
+
+    RemainderE = itemCount[4] % 2
+    QuotientE = itemCount[4] // 2
 
 
-    LenB = len(Bs)
+   LenB = itemCount[1]
     if LenB > 0:
         LenB -= QuotientE
     else:
         pass
-    RemainderB = LenB % 2
-    QuotientB = LenB // 2
+    RemainderB = itemCount[1] % 2
+    QuotientB = itemCount[1] // 2
 
     print(LenA, LenB, LenC, LenD, LenE)
     finalPrice = (SingularA * 50) + (offer150_QuotientA * 130) + (offer200_QuotientA * 200) + (RemainderB * 30) + (QuotientB * 45) + (LenC * 20) + (LenD * 15)  + (LenE * 40)
     return finalPrice
+
