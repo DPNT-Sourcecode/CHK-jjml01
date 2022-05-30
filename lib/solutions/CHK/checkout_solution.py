900c616b7b03303f357e483e5814ad191ccd2b93
sbx_user1051 <sbx_user1051@169.254.70.125>
2022-05-30 14:07:14 +0000

    Mon May 30 14:07:14 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,47 +1,57 @@
 
-
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    skuList = ['A','B','C','D','E','F']
+
+    # Initialisation, input handling
+
+    skuList = ['A' ,'B' ,'C' ,'D' ,'E' ,'F']
     itemList = [x for x in skus]
     for item in itemList:
         if item not in skuList:
             return -1
 
+    # Item handling
+
     itemBreakdown = [[0] for x in skuList]
 
     for item in skuList:
-        
-        itemBreakdown.append([x for x in itemList if x == skuList[skuList.index(item)]])
+
+        itemBreakdown[skuList.index(item)].append([x for x in itemList if x == skuList[skuList.index(item)]])
+        if len(itemBreakdown[skuList.index(item)]) > 1:
+            itemBreakdown[skuList.index(item)].pop(0)
 
     itemCount = [len(x) for x in itemBreakdown]
+
+    # Offers
 
     offer200_RemainderA = itemCount[0] % 5
     offer200_QuotientA = itemCount[0] // 5
 
-    SingularA = offer200_RemainderA % 3
+    offer150_RemainderA = offer200_RemainderA % 3
     offer150_QuotientA = offer200_RemainderA // 3
-
-
-
-    LenC = len(Cs)
-
-    LenD = len(Ds)
-
 
     RemainderE = itemCount[4] % 2
     QuotientE = itemCount[4] // 2
 
 
-   LenB = itemCount[1]
-    if LenB > 0:
-        LenB -= QuotientE
+
+    if itemCount[1] > 0:
+        itemCount[1] -= QuotientE
     else:
         pass
+
     RemainderB = itemCount[1] % 2
     QuotientB = itemCount[1] // 2
 
-    print(LenA, LenB, LenC, LenD, LenE)
-    finalPrice = (SingularA * 50) + (offer150_QuotientA * 130) + (offer200_QuotientA * 200) + (RemainderB * 30) + (QuotientB * 45) + (LenC * 20) + (LenD * 15)  + (LenE * 40)
+    RemainderF = itemCount[5] % 2
+    QuotientF = itemCount[5] // 2
+    itemCount[5] -= QuotientF
+
+    # Output
+
+    print(itemCount)
+    finalPrice = (offer150_RemainderA * 50) + (offer150_QuotientA * 130) + (offer200_QuotientA * 200) + (RemainderB * 30) + \
+                (QuotientB * 45) + (itemCount[2] * 20) + (itemCount[3] * 15) + (itemCount[4] * 40) + (itemCount[5] * 10)
     return finalPrice
+
